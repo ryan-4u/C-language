4535a6e1c98af4c6cfc4bfd8d3e0d1431b711f64
ryan-4u <aaryanofficial64@gmail.com>
2025-01-16 15:28:07 +0530

    Fix Bugs of 2ndlargest.c

--- array(1).c/2ndlargest.c
+++ array(1).c/2ndlargest.c
@@ -1,16 +1,19 @@
-<<<<<<< HEAD
 #include<stdio.h>
-int main(){
+int main()
+{
     int arr[5];
     int i,pr=1;
-    for(i=0;i<5;i++){
-     printf("enter element %d : \n",i+1);
+    for(i=0;i<5;i++)
+    {
+        printf("enter element %d : ",i+1);
         scanf("%d",&arr[i]);
     }
-        for(i=0;i<5;i++){
+    
+    for(i=0;i<5;i++)
+    {
         printf("%d  ",arr[i]);
-       }
-       //method 1
+    }
+    //method 1
     //  int max=arr[0];
     //  for(i=0;i<5;i++){
     //     if (max<arr[i])
@@ -22,51 +25,15 @@ int main(){
     //         smax=arr[i];
     //     }
     //     Method II
-     int max=arr[0],smax=arr[0];
-      for(i=0;i<5;i++){
-         if (max<arr[i]){
-             smax=max;
+    int max=arr[0],smax=arr[0];
+    for(i=0;i<5;i++)
+    {
+        if (max<arr[i]){
+            smax=max;
             max=arr[i];
-         }
-         else if(smax<arr[i] && max!=arr[i])
-         smax=arr[i];
-         }
-        printf("\nThe  second max value element of array : %d\n",smax);
-        return 0;
-=======
-#include<stdio.h>
-int main(){
-    int arr[5];
-    int i,pr=1;
-    for(i=0;i<5;i++){
-     printf("enter element %d : \n",i+1);
-        scanf("%d",&arr[i]);
+        }
+        else if(smax<arr[i] && max!=arr[i]) smax=arr[i];
     }
-        for(i=0;i<5;i++){
-        printf("%d  ",arr[i]);
-       }
-       //method 1
-    //  int max=arr[0];
-    //  for(i=0;i<5;i++){
-    //     if (max<arr[i])
-    //         max=arr[i];
-    //     }
-    //   int smax=arr[0];
-    //  for(i=0;i<5;i++){
-    //     if(arr[i]!=max && smax<arr[i])
-    //         smax=arr[i];
-    //     }
-    //     Method II
-     int max=arr[0],smax=arr[0];
-      for(i=0;i<5;i++){
-         if (max<arr[i]){
-             smax=max;
-            max=arr[i];
-         }
-         else if(smax<arr[i] && max!=arr[i])
-         smax=arr[i];
-         }
         printf("\nThe  second max value element of array : %d\n",smax);
         return 0;
->>>>>>> 3d47cd6e02e5d805c305c245734aa3903e9099f1
 }
